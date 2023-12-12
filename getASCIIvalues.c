e71bc52103bbf7a75e79fb50926d1b6140e1a9db
Gourav Sarkar <92428485+gouravsarkar053@users.noreply.github.com>
2023-12-12 17:23:22 +0530

    Create getASCIIvalues.c

---
+++ getASCIIvalues.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char c;
+    printf("Enter a character : "); //taking the input
+    scanf("%c" , &c);
+    printf("aASCII value of %c = %d",c,c); // the output
+
+    return 0;
+}
